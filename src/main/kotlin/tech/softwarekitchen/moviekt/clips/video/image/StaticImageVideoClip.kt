ad19526dedc6bf5fd1264e0f58ffc0433da45b5b
Tahlaria <tahlaria@software-kitchen.tech>
2023-03-24 11:16:35 +0100

    ADDED - image handling modes

--- src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/image/StaticImageVideoClip.kt
+++ src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/image/StaticImageVideoClip.kt
@@ -3,13 +3,22 @@ package tech.softwarekitchen.moviekt.clips.video.image
 import tech.softwarekitchen.common.vector.Vector2i
 import tech.softwarekitchen.moviekt.animation.position.SizeProvider
 import tech.softwarekitchen.moviekt.clips.video.VideoClip
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+enum class StaticImageMode{
+    KeepSize, Stretch
+}
+data class StaticImageVideoClipConfiguration(
+    val mode: StaticImageMode = StaticImageMode.KeepSize
+)
+
 class StaticImageVideoClip(
     size: SizeProvider,
     imageFile: File,
+    private val configuration: StaticImageVideoClipConfiguration = StaticImageVideoClipConfiguration(),
     tOffset: Float = 0f,
     visibilityDuration: Float? = null
 ): VideoClip(size, tOffset, visibilityDuration) {
@@ -17,8 +26,13 @@ class StaticImageVideoClip(
 
     override fun renderContent(frameNo: Int, nFrames: Int, tTotal: Float): BufferedImage {
         val img = generateEmptyImage(frameNo, nFrames, tTotal)
+
         val graphics = img.createGraphics()
-        graphics.drawImage(toDraw,0,0,null)
+        when(configuration.mode){
+            StaticImageMode.KeepSize -> graphics.drawImage(toDraw,0,0,null)
+            StaticImageMode.Stretch -> graphics.drawImage(toDraw.getScaledInstance(img.width,img.height, Image.SCALE_SMOOTH),0,0,null)
+        }
+
         return img
     }
 }
