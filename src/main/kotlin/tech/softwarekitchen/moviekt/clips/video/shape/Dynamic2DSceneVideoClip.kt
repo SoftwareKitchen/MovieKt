ea8eb59a5e8db12a1bdb27be942aed899fe027cc
Tahlaria <tahlaria@software-kitchen.tech>
2023-03-26 00:55:31 +0100

    ADDED - configurable shapes

--- src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/shape/Dynamic2DSceneVideoClip.kt
+++ src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/shape/Dynamic2DSceneVideoClip.kt
@@ -2,12 +2,15 @@ package tech.softwarekitchen.moviekt.clips.video.shape
 
 import tech.softwarekitchen.moviekt.animation.position.SizeProvider
 import tech.softwarekitchen.moviekt.clips.video.VideoClip
+import java.awt.BasicStroke
+import java.awt.Color
 import java.awt.Shape
 import java.awt.image.BufferedImage
 
+data class ShapePaintConfiguration(val shape: Shape, val strokeColor: Color, val fillColor: Color?, val strokeWidth: Int = 1)
 class Dynamic2DSceneVideoClip(
     size: SizeProvider,
-    private val shapeProvider: (Int, Int, Float) -> List<Shape>,
+    private val shapeProvider: (Int, Int, Float) -> List<ShapePaintConfiguration>,
     tOffset: Float = 0f,
     visibilityDuration: Float? = null
 ) : VideoClip(size,tOffset,visibilityDuration){
@@ -15,7 +18,14 @@ class Dynamic2DSceneVideoClip(
         val img = generateEmptyImage(frameNo, nFrames, tTotal)
         val graphics = img.createGraphics()
         shapeProvider(frameNo, nFrames, tTotal).forEach{
-            graphics.draw(it)
+            shape ->
+            shape.fillColor?.let{
+                graphics.color = it
+                graphics.fill(shape.shape)
+            }
+            graphics.color = shape.strokeColor
+            graphics.stroke = BasicStroke(shape.strokeWidth.toFloat())
+            graphics.draw(shape.shape)
         }
         return img
     }
