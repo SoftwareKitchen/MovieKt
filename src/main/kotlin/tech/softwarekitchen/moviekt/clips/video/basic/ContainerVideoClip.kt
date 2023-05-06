c727c42019fc124b5421df8c24ee664b4b42ba6e
Tahlaria <tahlaria@software-kitchen.tech>
2023-05-06 09:23:18 +0200

    ADDED - optional border for container clip, ADDED - custom TTF font for static text clip

--- src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/basic/ContainerVideoClip.kt
+++ src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/basic/ContainerVideoClip.kt
@@ -5,6 +5,8 @@ import tech.softwarekitchen.moviekt.animation.position.SizeProvider
 import tech.softwarekitchen.moviekt.clips.video.VideoClip
 import tech.softwarekitchen.moviekt.exception.ChainedClipRequiresVisibilityDurationException
 import tech.softwarekitchen.moviekt.exception.ClipSizeMismatchException
+import java.awt.BasicStroke
+import java.awt.Color
 import java.awt.image.BufferedImage
 
 fun VideoClip.chain(other: VideoClip): ContainerVideoClip{
@@ -26,15 +28,33 @@ fun VideoClip.chain(other: VideoClip): ContainerVideoClip{
     return container
 }
 
+data class ContainerBorderConfiguration(
+    val width: Int = 0,
+    val color: Color = Color.WHITE
+)
+data class ContainerVideoClipConfiguration(
+    val border: ContainerBorderConfiguration = ContainerBorderConfiguration()
+)
+
 class ContainerVideoClip(
     size: SizeProvider,
     tOffset: Float = 0f,
-    visibilityDuration: Float? = null
+    visibilityDuration: Float? = null,
+    private val configuration: ContainerVideoClipConfiguration = ContainerVideoClipConfiguration()
 ): VideoClip(
     size, tOffset, visibilityDuration
 ) {
     override fun renderContent(frameNo: Int, nFrames: Int, tTotal: Float): BufferedImage {
         val img = BufferedImage(size(frameNo, nFrames, tTotal).x,size(frameNo, nFrames, tTotal).y,BufferedImage.TYPE_INT_ARGB)
+
+        if(configuration.border.width > 0){
+            val size = getSize(frameNo, nFrames, tTotal)
+            val graphics = img.createGraphics()
+            graphics.stroke = BasicStroke(configuration.border.width.toFloat())
+            graphics.color = configuration.border.color
+            graphics.drawRect(0,0, size.x, size.y)
+        }
+
         return img
     }
 }
--- src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/text/StaticTextVideoClip.kt
+++ src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/text/StaticTextVideoClip.kt
@@ -4,21 +4,46 @@ import tech.softwarekitchen.common.vector.Vector2i
 import tech.softwarekitchen.moviekt.animation.position.SizeProvider
 import tech.softwarekitchen.moviekt.clips.video.VideoClip
 import java.awt.Color
+import java.awt.Dimension
+import java.awt.Font
+import java.awt.Graphics2D
+import java.awt.font.FontRenderContext
+import java.awt.geom.AffineTransform
+import java.awt.geom.Rectangle2D
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_ARGB
+import java.io.File
 
 class StaticTextVideoClipConfiguration(
     val fontSize: Int = 24,
-    val color: Color = Color.BLACK
+    val color: Color = Color.BLACK,
+    val ttFont: File? = null
 )
 class StaticTextVideoClip (size: SizeProvider, private val text: String, private val configuration: StaticTextVideoClipConfiguration = StaticTextVideoClipConfiguration(), tOffset: Float = 0f, visibilityDuration: Float? = null): VideoClip(size, tOffset, visibilityDuration) {
+    private val font: Font?
+
+    init{
+        font = configuration.ttFont?.let{
+            Font.createFont(Font.TRUETYPE_FONT, it)
+        }
+    }
+
+    fun getTextSize(text: String): Rectangle2D {
+        val f = font ?: run {
+            val img = BufferedImage(100,100,TYPE_INT_ARGB)
+            val graphics = img.createGraphics()
+            graphics.font!!
+        }
+
+        return f.deriveFont(configuration.fontSize.toFloat()).getStringBounds(text, FontRenderContext(AffineTransform(), true, true))
+    }
 
     override fun renderContent(frameNo: Int, nFrames: Int, t: Float): BufferedImage {
         val curSize = size(frameNo, nFrames, t)
         val img = BufferedImage(curSize.x,curSize.y,TYPE_INT_ARGB)
 
         val graphics = img.createGraphics()
-        graphics.font = graphics.font.deriveFont(configuration.fontSize.toFloat())
+        graphics.font = (font ?: graphics.font).deriveFont(configuration.fontSize.toFloat())
         graphics.color = Color(0,0,0,0)
         graphics.fillRect(0,0,curSize.x,curSize.y)
         graphics.color = configuration.color
