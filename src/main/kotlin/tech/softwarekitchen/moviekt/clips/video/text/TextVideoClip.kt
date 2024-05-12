5067bf03b3c542b43ddd5fabbd90affb3c700be6
Tahlaria <tahlaria@software-kitchen.tech>
2024-05-12 18:58:04 +0200

    some text clips

--- src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/text/TextVideoClip.kt
+++ src/main/kotlin/tech/softwarekitchen/moviekt/clips/video/text/TextVideoClip.kt
@@ -93,3 +93,114 @@ class TextVideoClip (
 fun String.getTextSize(f: Font): Rectangle2D{
     return f.getStringBounds(this, FontRenderContext(AffineTransform(), true, true))
 }
+
+class DynamicTextVideoClipConfiguration(
+    val callback: (VideoTimestamp) -> String,
+    val fontSize: Int = 24,
+    val color: Color = Color.BLACK,
+    val ttFont: File? = null,
+    val anchor: TextAnchor = TextAnchor.Left
+)
+open class DynamicTextVideoClip (
+    id: String,
+    size: Vector2i,
+    position: Vector2i,
+    visible: Boolean,
+    private val configuration: DynamicTextVideoClipConfiguration
+) : VideoClip(id, size, position, visible, volatile = true) {
+
+    companion object{
+        val PropertyKey_Text = "Text"
+    }
+    override val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    private val fontColorProperty = VideoClipThemeProperty(VideoTheme.VTPropertyKey_FontColor, configuration.color, this::markDirty)
+    private val fontSizeProperty = VideoClipThemeProperty(VideoTheme.VTPropertyKey_FontSize, configuration.fontSize, this::markDirty)
+    private val fontProperty = VideoClipThemeProperty(VideoTheme.VTPropertyKey_Font, configuration.ttFont?.let(this::loadFont), this::markDirty){
+        when{
+            it is Font -> it
+            it is File -> loadFont(it)
+            else -> throw Exception()
+        }
+    }
+
+    init{
+        registerProperty(fontProperty)
+        registerProperty(fontColorProperty)
+        registerProperty(fontSizeProperty)
+    }
+
+    private fun loadFont(file: File): Font{
+        return Font.createFont(Font.TRUETYPE_FONT, file)
+    }
+
+    fun getTextSize(text: String): Rectangle2D {
+        val f = fontProperty.v ?: run {
+            val img = BufferedImage(100,100,TYPE_INT_ARGB)
+            val graphics = img.createGraphics()
+            graphics.font!!
+        }
+
+        return f.deriveFont(fontSizeProperty.v.toFloat()).getStringBounds(text, FontRenderContext(AffineTransform(), true, true))
+    }
+
+    override fun renderContent(img: BufferedImage, t: VideoTimestamp) {
+        val curSize = Vector2i(img.width, img.height)
+        val text = configuration.callback(t)
+
+        val graphics = img.createGraphics()
+        val font = (fontProperty.v ?: graphics.font).deriveFont(fontSizeProperty.v.toFloat())
+        val bounds = font.getStringBounds(text,graphics.fontRenderContext)
+        val topleft = when(configuration.anchor){
+            TextAnchor.Center -> curSize.scale(0.5).plus(Vector2i(- bounds.width.toInt() / 2,- bounds.height.toInt() / 2))
+            TextAnchor.Left -> Vector2i( 0, curSize.y / 2 - bounds.height.toInt() / 2)
+        }
+        val mapped = Vector2i(topleft.x - bounds.x.toInt() / 2, topleft.y - bounds.y.toInt())
+
+        graphics.font = font
+        graphics.color = fontColorProperty.v
+
+        graphics.drawString(text,mapped.x, mapped.y)
+    }
+}
+
+class TextTimerVideoClipConfiguration(
+    val fontSize: Int = 24,
+    val color: Color = Color.BLACK,
+    val ttFont: File? = null,
+    val anchor: TextAnchor = TextAnchor.Left
+){
+    fun toDynammic(): DynamicTextVideoClipConfiguration{
+        return DynamicTextVideoClipConfiguration(
+            {
+                val sTot = it.t.toInt()
+                val min = (sTot / 60) % 60
+                val sec = sTot % 60
+                val h = sTot / 3600
+
+                val sStr = "$sec".padStart(2, '0')
+                if(h > 0){
+                    val mStr = "$min".padStart(2, '0')
+                    "$h:$mStr:$sStr"
+                }else{
+                    "$min:$sStr"
+
+                }
+            },
+            fontSize, color, ttFont, anchor
+        )
+    }
+}
+
+
+class TextTimerVideoClip(
+    id: String,
+    size: Vector2i,
+    position: Vector2i,
+    visible: Boolean,
+    configuration: TextTimerVideoClipConfiguration
+): DynamicTextVideoClip(
+    id, size, position, visible, configuration.toDynammic()
+){
+
+}
